94705a5504cb77f7f47911a0a36d68516296804d
wsz <1175690167@qq.com>
2023-06-28 21:06:38 +0800

    feat: [dependencies] 引入pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import router from './router/index'
 // import './style.css'
+import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
@@ -9,5 +10,5 @@ const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
-
+app.use(createPinia())
 app.mount('#app')
