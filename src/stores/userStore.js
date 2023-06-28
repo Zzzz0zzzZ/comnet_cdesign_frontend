42fc70210e465fe4b4c17f0219400cc2744c82ee
wsz <1175690167@qq.com>
2023-06-29 01:55:26 +0800

    feat: [stores] 添加用户信息store

---
+++ src/stores/userStore.js
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia"
+
+export const useUserStore = defineStore('userStore', {
+    state: () => {
+        return {
+            userinfo: {}
+        }
+    },
+    actions: {
+
+    },
+    getters: {
+
+    }
+})
